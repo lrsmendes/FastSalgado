990bdf56e5cf2e323456533797827fb4ca13ee0e
Lucas Raziel <101843880+lrsmendes@users.noreply.github.com>
2024-05-05 16:38:59 -0300

    Add files via upload

---
+++ php/login_processar.php
@@ -0,0 +1,38 @@
+<?php
+$email = $_REQUEST["email"];
+$senha = $_REQUEST["senha"];
+
+try {
+    $conn = new PDO("mysql:host=localhost;dbname=fastimoveis;charset=utf8mb4", "root", "");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Criptografa a senha fornecida pelo usuário usando MD5
+    $senha_md5 = md5($senha);
+
+    $verificarUsuario = "SELECT * FROM usuarios WHERE email=? AND senha=?";
+    $stmt = $conn->prepare($verificarUsuario);
+    // Usa a senha criptografada em MD5
+    $stmt->execute([$email, $senha_md5]);
+    $row = $stmt->fetch();
+
+    if ($row) {
+        $nomeUsuario = $row["nome"];
+        session_start();
+        $_SESSION["nomeUsuario"] = $nomeUsuario;
+
+        // Verifica se é conta de administrador
+        if ($email === "adminbr@gmail.com") {
+            header("Location: painel.php");
+        } else {
+            header("Location: painel.php");
+        }
+    } else {
+        echo "Login falhou. Verifique suas credenciais. <a href='login.php'>Tentar novamente</a>";
+    }
+
+    $stmt = null;
+    $conn = null;
+} catch (PDOException $e) {
+    echo "Erro: " . $e->getMessage();
+}
+?>
